d431c7960822ac1200de03377e25cfb369146c89
aimers1975 <amy_hindman@yahoo.com>
2015-07-06 05:49:07 -0500

    attempting to start eclipse project

---
+++ buildmatrix.bat
@@ -0,0 +1,3 @@
+javac C:\MCCTeamProject\ParallelMatrix\src\Matrix.java
+javac -cp C:\MCCTeamProject\ParallelMatrix\src;C:\MCCTeamProject\ParallelMatrix\tests\junit-4.11.jar C:\MCCTeamProject\ParallelMatrix\tests\MatrixTest.java
+java -cp C:\MCCTeamProject\ParallelMatrix\src;C:\MCCTeamProject\ParallelMatrix\tests;C:\MCCTeamProject\ParallelMatrix\tests\junit-4.11.jar;C:\MCCTeamProject\ParallelMatrix\tests\hamcrest-core-1.3.jar;. org.junit.runner.JUnitCore MatrixTest
